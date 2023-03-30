c90effa82eef127db003cdcf663dfe62a8b996c6
Cristian Rodrigues <89823954+CristianRsouza@users.noreply.github.com>
2023-03-30 20:33:44 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,33 +3,34 @@ import './App.css'
 
 function App() {
   const inputRef = useRef()
+  
   const [color, setColor] = useState()
 
-  const handleToBold = () => {
-    inputRef.current.classList.toggle('bold')
-  }
-  const handleToItalic = () => {
-    inputRef.current.classList.toggle('italic')
-  }
-  const handleToUPPER = () => {
-    inputRef.current.value = inputRef.current.value.toUpperCase()
-  }
-  const handleToMin = () => {
-    inputRef.current.value = inputRef.current.value.toLowerCase()
-  }
-  const HandleColor = (e) => {
-      setColor(e.target.value)
-      inputRef.current.style.color = color
-  }
+      const handleToBold = () => {
+        inputRef.current.classList.toggle('bold')
+      }
+      const handleToItalic = () => {
+        inputRef.current.classList.toggle('italic')
+      }
+      const handleToUPPER = () => {
+        inputRef.current.value = inputRef.current.value.toUpperCase()
+      }
+      const handleToMin = () => {
+        inputRef.current.value = inputRef.current.value.toLowerCase()
+      }
+      const HandleColor = (e) => {
+          setColor(e.target.value)
+          inputRef.current.style.color = color
+      }
 
   return (
     <div className='Container'>
       <div className='box'>
         <div className='header'>
           <button onClick={handleToBold}>B</button>
-          <button onClick={handleToItalic}>I</button>
-          <button onClick={handleToUPPER}>U</button>
-          <button onClick={handleToMin}>M</button>
+            <button onClick={handleToItalic}>I</button>
+             <button onClick={handleToUPPER}>U</button>
+              <button onClick={handleToMin}>M</button>
           <input type="color" onChange={HandleColor}/>
         </div>
         <div className='Input'>
